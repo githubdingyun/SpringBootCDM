95249954d2a36831c67fa61c873877dc1ff2ae84
DinGYun <1492417758@qq.com>
2018-08-11 14:30:40 +0800

    Initial commit

---
+++ src/main/java/com/dingyun/springbootcdm/SpringbootcdmApplication.java
@@ -0,0 +1,12 @@
+package com.dingyun.springbootcdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcdmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootcdmApplication.class, args);
+    }
+}
